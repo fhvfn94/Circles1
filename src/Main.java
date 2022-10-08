13e0eb792f6a9df68aaec9e4a6e8743066a9f55f
fhvfn94 <fhvfn94@gmail.com>
2022-10-08 12:47:01 +0300

    add task 4

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         //task1();
         //task2();
-        task3();
+        //task3();
+        task4();
 
     }
     public static void task1(){
@@ -22,5 +23,11 @@ public class Main {
             }
         }
     }
+    public static void task4(){
+        for (int i = 10; i > -11; i--){
+            System.out.println(i);
+        }
+    }
+
 
 }
